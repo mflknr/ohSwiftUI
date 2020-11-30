f991fc6b10c38a2a800f49e8e596e02d20910d97
nihilias <nihilias@icloud.com>
2020-11-30 23:10:50 +0100

    chore: create basic swiftui xcode project

---
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Created by nihilias on 30.11.20.
+// Copyright © 2020 nihilias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by nihilias on 30.11.20.
+// Copyright © 2020 nihilias. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
