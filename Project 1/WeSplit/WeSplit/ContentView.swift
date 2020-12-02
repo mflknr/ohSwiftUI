8b6b3d9c780d07cdc0a4a295cf44ca0931678c72
nihilias <nihilias@icloud.com>
2020-12-03 00:21:39 +0100

    feat: add necessary ui

--- Project 1/WeSplit/WeSplit/ContentView.swift
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -6,21 +6,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         NavigationView {
             Form {
-                // mutating state
-                Section(
-                    header: Text("Mutating a state")
-                ) {
-                    Button("Tap again after \(tapCount) times!") {
-                        self.tapCount += 1
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker(
+                        "Number of People",
+                        selection: $numberOfPeople
+                    ) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
                     }
                 }
+                
+                Section(
+                    header: Text("How much tip do you want to leave?")
+                ) {
+                    Picker(
+                        "Tip Percentage",
+                        selection: $tipPercentage
+                    ) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-            .navigationTitle("SwiftUI - First Steps")
+            .navigationTitle("WeSplit")
         }
     }
 }
