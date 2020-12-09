37c26312ca1b0a43421b5140a0765529af38dbdc
nihilias <m.hoetten-loens@nanogiants.de>
2020-12-09 10:14:42 +0100

    chore: create challenge day project

---
+++ Challenge Day/ChallengeDay/ChallengeDayApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by ___ORGANIZATIONNAME___ on 09.12.20.
+// 
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge Day/ChallengeDay/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Created by ___ORGANIZATIONNAME___ on 09.12.20.
+// 
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
